ae30ca0578eab3a95dfbaba55a5908f5940a2828
Prsoderlund <pompe.soderlund@gmail.com>
2023-05-24 10:32:34 +0200

    Chat

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import javax.swing.JOptionPane;
+
+public class Main {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "ehehj", "Error", JOptionPane.ERROR_MESSAGE);
+    }
+}
